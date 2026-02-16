b9abf48caf669c496a5d0eaf175e387cd2dc08b6
abdullahi-mohammed <moh.abdullahi2003@gmail.com>
2026-02-16 13:34:04 +0100

    done with week 6 ass

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,6 +13,9 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data)
+            return; // No duplicates allowed
+
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +37,31 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        if (value < Data)
+        {
+            // Search to the left
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else
+        {
+            // Search to the right
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
